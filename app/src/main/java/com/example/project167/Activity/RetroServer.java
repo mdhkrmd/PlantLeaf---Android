9b5eeda6d2f0a0dd01f1377e8f66889bd3b74c5c
Andhika R <dhikramadhan008@gmail.com>
2024-03-21 12:09:01 +0700

    minor

--- app/src/main/java/com/example/project167/Activity/RetroServer.java
+++ app/src/main/java/com/example/project167/Activity/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    public static final String BASE_URL = "https://249d-2001-448a-2061-c58a-79a6-2f7c-898c-5078.ngrok-free.app";
+    public static final String BASE_URL = "https://ffdc-2001-448a-2061-c58a-9932-c924-c845-8d4d.ngrok-free.app";
 
     public static RetrofitAPI getRetrofitAPI() {
         return new Retrofit.Builder()
