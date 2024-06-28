677aa07891f47fa0ffe8fc23605ac36cffff6acc
unknown <dhikramadhan008@gmail.com>
2024-06-28 13:21:37 +0700

    static domain ngrok

--- app/src/main/java/com/example/project167/Activity/RetroServer.java
+++ app/src/main/java/com/example/project167/Activity/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    public static final String BASE_URL = "https://ffc3-36-79-46-171.ngrok-free.app";
+    public static final String BASE_URL = "https://hookworm-meet-strangely.ngrok-free.app";
 
     public static RetrofitAPI getRetrofitAPI() {
         return new Retrofit.Builder()
